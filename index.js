e65c89dc4223a1e4a5eb12c85225e66949830aad
Abin Simon <abinsimon10@gmail.com>
2020-05-31 13:37:43 +0530

    maybe don't slow down

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const logo = document.getElementsByTagName("svg")[0];
 
 let xdir = 0.3;
 let ydir = 0.3;
-const speed = 1;
 function step(ts) {
   progress = ts - prev;
   prev = ts;
@@ -18,28 +17,8 @@ function step(ts) {
   newTop += progress * xdir;
   newLeft += progress * ydir;
 
-  if (newTop + 100 > window.innerHeight) {
-    xdir += 0.1 * speed;
-    xdir = -Math.abs(xdir);
-  } else if (newTop < 0) {
-    xdir = Math.abs(xdir);
-    xdir += 0.1 * speed;
-  }
-
-  if (newLeft + 200 > window.innerWidth) {
-    ydir += 0.1 * speed;
-    ydir = -Math.abs(ydir);
-  } else if (newLeft < 0) {
-    ydir = Math.abs(ydir);
-    ydir += 0.1 * speed;
-  }
-
-  if (xdir > 10) {
-    xdir = 0.1;
-    ydir = 0.1;
-    logo.style.top = "0px";
-    logo.style.left = "0px";
-  }
+  if (newTop + 100 > window.innerHeight || newTop < 0) xdir = -xdir;
+  if (newLeft + 200 > window.innerWidth || newLeft < 0) ydir = -ydir;
 
   logo.style.top = `${newTop}px`;
   logo.style.left = `${newLeft}px`;
